f2ca7319c8d8a1c457eb61455a5ab046259b80af
Anurag Samarth <asamarth017@gmail.com>
2024-05-27 15:25:47 +0530

    cart page is ready and delete button is also ready

--- cartPage.js
+++ cartPage.js
@@ -16,19 +16,19 @@ export const cartPage = async () => {
      <div class="cart-product-img">
        <img src=${element.image} alt="">
      </div>
-     <h4>${element.title}</h4>
+     <h4 class="cart-product-text">${element.title}</h4>
      <div>
-       <div class="product-quantity" data-product-id="${element._id}">
+       <div class="product-quantity" data-product-id="${element.id}">
            <button class="btns decrement">-</button>
            <p class="count">${element.quantity}</p>
            <button class="btns increment">+</button>
        </div>
      </div>
      <div>
-       <h4>₹ ${element.price}</h4>
+       <h4 class="cart-product-price">₹ ${element.price}</h4>
    </div>
      <div>
-       <button class="delete-btn" data-product-id="${element._id}">Delete Product</button>
+       <button class="delete-btn" data-product-id="${element.id}">Delete Product</button>
        <button class="product-buy-now btn buy">Buy Now</button>
    </div>
  </div>
@@ -37,12 +37,28 @@ export const cartPage = async () => {
     });
     
     const quantites = document.querySelectorAll(".product-quantity");
+    const deleteBtns = document.querySelectorAll(".delete-btn");
     
     quantites.forEach((elem) => {
         elem.addEventListener("click", (e) => {
         productsQuantity(e);
     });
   });
+
+  deleteBtns.forEach((elem)=>{
+    elem.addEventListener('click', (e)=>{
+      console.log(e.target);
+      const productId = e.target.dataset.productId;
+     
+     const productArrayElem = productArray.filter((product)=> productId !== product.id)
+
+     console.log(productArrayElem)
+     localStorage.setItem('productArray', JSON.stringify(productArrayElem))
+
+     const productElement = e.target.closest('.cart-product'); 
+    productElement.remove();
+    })
+  })
 };
 
 cartPage();
